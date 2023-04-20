dd78fcf0a774c5d0ba50d1fe04fb5bd79b41b7ba
Mihail Majukin <m.v.majukin@mail.ru>
2023-04-20 03:17:01 +0300

    add models and diagrams

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      dropDups: true,
+      validate: {
+        validator: (link) => isEmail(link),
+        message: 'Неправильный формат почты',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      minLength: 2,
+      maxLength: 30,
+      required: true,
+    },
+  },
+  { versionKey: false },
+);
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }, { runValidators: true })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(
+          new UnauthorizedError('Неправильные почта или пароль'),
+        );
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new UnauthorizedError('Неправильные почта или пароль'),
+          );
+        }
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model('User', userSchema);
